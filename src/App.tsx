3b2c208f20d09135e714a2e9dab60854a58f03ba
CedricPradels <cedric.pradels@gmail.com>
2020-05-08 21:24:13 +0200

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Offer from "./pages/Offer";
+import Offers from "./pages/Offers";
+import Order from "./pages/Order";
+import Publish from "./pages/Publish";
+import Register from "./pages/Register";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/publish">
+            <Publish />
+          </Route>
+          <Route path="/order/:id">
+            <Order />
+          </Route>
+          <Route path="/offer/:id">
+            <Offer />
+          </Route>
+          <Route path="/">
+            <Offers />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
